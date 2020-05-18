7a9cd7985a8c28304453dbdcc9257f0494703787
Heath Mast <hrmast@gmail.com>
2020-05-18 01:42:39 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,9 +3,14 @@ class Shoe
   attr_reader :brand
 
   BRANDS = []
-  
+
   def initialize(brand)
     @brand = brand
+  end
+
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
   end
 
   def cobble
